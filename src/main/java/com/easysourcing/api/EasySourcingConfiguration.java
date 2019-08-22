bdafa222637b6d81bddb8ee1343b8182d78ec53c
Ali Kelleci <a.kelleci@portbase.com>
2019-08-22 22:20:52 +0200

    - Spring Boot autoconfiguration support added

---
+++ src/main/java/com/easysourcing/api/EasySourcingConfiguration.java
@@ -0,0 +1,37 @@
+package com.easysourcing.api;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.MethodParameterScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Map;
+
+@ComponentScan("com.easysourcing.api")
+public class EasySourcingConfiguration {
+
+  @Autowired
+  private ApplicationContext applicationContext;
+
+  private String getHostPackageName() {
+    Map<String, Object> annotatedBeans = applicationContext.getBeansWithAnnotation(SpringBootApplication.class);
+    return annotatedBeans.isEmpty() ? null : annotatedBeans.values().toArray()[0].getClass().getPackage().getName();
+  }
+
+
+  @Bean
+  public Reflections reflections() {
+    return new Reflections(getHostPackageName(),
+        new TypeAnnotationsScanner(),
+        new SubTypesScanner(),
+        new MethodAnnotationsScanner(),
+        new MethodParameterScanner()
+    );
+  }
+}
