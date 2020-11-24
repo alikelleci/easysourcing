a1fe2a76f54b51d3dc389ccc4cd939395a8f680e
Ali Kelleci <a.kelleci@portbase.com>
2020-11-24 08:57:20 +0100

    objectmapper is now singleton

--- src/main/java/com/github/easysourcing/support/JacksonUtils.java
+++ src/main/java/com/github/easysourcing/support/JacksonUtils.java
@@ -7,20 +7,26 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
-import lombok.experimental.UtilityClass;
 
-@UtilityClass
 public class JacksonUtils {
 
-  public ObjectMapper enhancedObjectMapper() {
-    return new ObjectMapper()
-        .registerModule(new ParameterNamesModule())
-        .registerModule(new Jdk8Module())
-        .registerModule(new JavaTimeModule())
-        .configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false)
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        .configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false)
-        .configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
-        .configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+  private static ObjectMapper objectMapper;
+
+  private JacksonUtils() {
+  }
+
+  public static ObjectMapper enhancedObjectMapper() {
+    if (objectMapper == null) {
+      objectMapper = new ObjectMapper()
+          .registerModule(new ParameterNamesModule())
+          .registerModule(new Jdk8Module())
+          .registerModule(new JavaTimeModule())
+          .configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false)
+          .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+          .configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false)
+          .configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
+          .configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+    }
+    return objectMapper;
   }
 }
