46e3fde3c7e7ea0561b421e5405629e9e68b8410
Ali Kelleci <a.kelleci@portbase.com>
2021-07-05 13:17:14 +0200

    minor changes

---
+++ easysourcing/src/main/java/io/github/alikelleci/easysourcing/messages/MessageTransformer.java
@@ -0,0 +1,45 @@
+package io.github.alikelleci.easysourcing.messages;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.github.alikelleci.easysourcing.messages.Message;
+import io.github.alikelleci.easysourcing.util.JacksonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.kafka.streams.kstream.ValueTransformer;
+import org.apache.kafka.streams.processor.ProcessorContext;
+
+@Slf4j
+public class MessageTransformer implements ValueTransformer<JsonNode, Message<?>> {
+
+  private ProcessorContext context;
+
+  private final Class<? extends Message<?>> type;
+
+  public MessageTransformer(Class<? extends Message<?>> type) {
+    this.type = type;
+  }
+
+  @Override
+  public void init(ProcessorContext processorContext) {
+    this.context = processorContext;
+  }
+
+  @Override
+  public Message<?> transform(JsonNode jsonNode) {
+    try {
+      Message<?> message = JacksonUtils.enhancedObjectMapper().convertValue(jsonNode, type);
+      message.getMetadata().injectContext(context);
+      return message;
+    } catch (Exception e) {
+      log.error("Error deserializing JSON", ExceptionUtils.getRootCause(e));
+      return null;
+    }
+  }
+
+  @Override
+  public void close() {
+
+  }
+
+
+}
