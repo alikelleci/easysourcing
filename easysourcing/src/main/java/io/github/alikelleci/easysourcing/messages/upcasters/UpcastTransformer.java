3496a4e6576b5706f36536c3a9b3529d89eb8b22
akelleci <B>
2021-07-05 03:34:37 +0200

    minor change

--- easysourcing/src/main/java/io/github/alikelleci/easysourcing/messages/upcasters/UpcastTransformer.java
+++ easysourcing/src/main/java/io/github/alikelleci/easysourcing/messages/upcasters/UpcastTransformer.java
@@ -47,7 +47,7 @@ public class UpcastTransformer implements ValueTransformer<JsonNode, JsonNode> {
           int targetRevision = handler.getMethod().getAnnotation(Upcast.class).revision();
 
           if (sourceRevision == targetRevision) {
-            ObjectNode payload = (ObjectNode) handler.invoke(jsonNode, context);
+            JsonNode payload = handler.invoke(jsonNode, context);
             ((ObjectNode) jsonNode).set("payload", payload);
             ((ObjectNode) jsonNode.get("metadata").get("entries")).put("$revision", sourceRevision + 1);
           }
