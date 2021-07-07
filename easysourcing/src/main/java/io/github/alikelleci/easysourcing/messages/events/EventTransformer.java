f1462454bcf6679f62ec12ffba83adbf66fa1987
akelleci <B>
2021-07-07 02:40:12 +0200

    minor change

--- easysourcing/src/main/java/io/github/alikelleci/easysourcing/messages/events/EventTransformer.java
+++ easysourcing/src/main/java/io/github/alikelleci/easysourcing/messages/events/EventTransformer.java
@@ -1,31 +1,23 @@
 package io.github.alikelleci.easysourcing.messages.events;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.github.alikelleci.easysourcing.messages.Metadata;
-import io.github.alikelleci.easysourcing.messages.upcasters.Upcaster;
-import io.github.alikelleci.easysourcing.messages.upcasters.annotations.Upcast;
 import io.github.alikelleci.easysourcing.util.JsonUtils;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.streams.kstream.ValueTransformer;
 import org.apache.kafka.streams.processor.ProcessorContext;
 
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.Optional;
 
 
 public class EventTransformer implements ValueTransformer<JsonNode, Void> {
 
   private ProcessorContext context;
-
-  private final MultiValuedMap<String, Upcaster> upcasters;
   private final MultiValuedMap<Class<?>, EventHandler> eventHandlers;
 
-  public EventTransformer(MultiValuedMap<String, Upcaster> upcasters, MultiValuedMap<Class<?>, EventHandler> eventHandlers) {
-    this.upcasters = upcasters;
+  public EventTransformer(MultiValuedMap<Class<?>, EventHandler> eventHandlers) {
     this.eventHandlers = eventHandlers;
   }
 
@@ -36,8 +28,6 @@ public class EventTransformer implements ValueTransformer<JsonNode, Void> {
 
   @Override
   public Void transform(JsonNode jsonEvent) {
-    upcastIfNeeded(jsonEvent);
-
     Object event = JsonUtils.toJavaType(jsonEvent);
     if (event == null) {
       return null;
@@ -63,33 +53,5 @@ public class EventTransformer implements ValueTransformer<JsonNode, Void> {
 
   }
 
-
-  private JsonNode upcastIfNeeded(JsonNode jsonNode) {
-    String className = Optional.ofNullable(jsonNode)
-        .map(node -> node.get("@class"))
-        .map(JsonNode::textValue)
-        .orElse(null);
-
-    if (StringUtils.isBlank(className)) {
-      return null;
-    }
-
-    Collection<Upcaster> handlers = upcasters.get(className);
-    if (CollectionUtils.isEmpty(handlers)) {
-      return jsonNode;
-    }
-
-    Metadata metadata = Metadata.builder().build().injectContext(context);
-
-    handlers.stream()
-        .sorted(Comparator.comparingInt(handler -> handler.getMethod().getAnnotation(Upcast.class).revision()))
-        .forEach(handler -> {
-          ObjectNode result = (ObjectNode) handler.invoke(jsonNode, metadata);
-          ((ObjectNode) jsonNode).removeAll();
-          ((ObjectNode) jsonNode).setAll(result);
-        });
-
-    return jsonNode;
-  }
 
 }
