da600c2938c2cc1bc708d1a9a5a868378da1dfc4
Tuna <B>
2021-07-14 20:51:12 +0200

    minor changes

--- easysourcing/src/main/java/io/github/alikelleci/easysourcing/messages/commands/RequestReplyGateway.java
+++ easysourcing/src/main/java/io/github/alikelleci/easysourcing/messages/commands/RequestReplyGateway.java
@@ -1,19 +1,15 @@
 package io.github.alikelleci.easysourcing.messages.commands;
 
 import io.github.alikelleci.easysourcing.messages.Metadata;
-import io.github.alikelleci.easysourcing.support.serializer.JsonDeserializer;
+import io.github.alikelleci.easysourcing.util.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.header.Header;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.thavam.util.concurrent.blockingMap.BlockingHashMap;
 import org.thavam.util.concurrent.blockingMap.BlockingMap;
 
@@ -21,8 +17,6 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Optional;
-import java.util.Properties;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
@@ -36,14 +30,15 @@ public class RequestReplyGateway extends CommandGateway {
   private boolean running = true;
 
 
-  public RequestReplyGateway(Producer<String, Object> producer, String replyTopic) {
+  public RequestReplyGateway(Producer<String, Object> producer, Consumer<String, CommandResult> consumer, String replyTopic) {
     super(producer);
+    this.consumer = consumer;
     this.replyTopic = replyTopic;
+
     startConsumer(replyTopic);
   }
 
   private void startConsumer(String replyTopic) {
-    consumer = new KafkaConsumer<>(properties(), new StringDeserializer(), new JsonDeserializer<>(CommandResult.class));
     consumer.subscribe(Collections.singletonList(replyTopic));
 
     Thread thread = new Thread(() -> {
@@ -71,11 +66,17 @@ public class RequestReplyGateway extends CommandGateway {
     thread.start();
   }
 
-  protected Future<CommandResult> sendAndWait(ProducerRecord<String, Object> record) {
+
+  @Override
+  protected Future<RecordMetadata> send(ProducerRecord<String, Object> record) {
     record.headers()
         .remove(Metadata.REPLY_TO)
         .add(Metadata.REPLY_TO, replyTopic.getBytes(StandardCharsets.UTF_8));
 
+    return super.send(record);
+  }
+
+  protected Future<CommandResult> sendAndWait(ProducerRecord<String, Object> record) {
     this.send(record);
 
     return CompletableFuture.supplyAsync(() -> {
@@ -93,19 +94,26 @@ public class RequestReplyGateway extends CommandGateway {
     });
   }
 
-  private Properties properties() {
-    Properties properties = new Properties();
-    properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-    properties.put(ConsumerConfig.GROUP_ID_CONFIG, "command-gateway-" + UUID.randomUUID().toString());
-    properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-    properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-    properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-    properties.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
-    properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-    properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1);
-    properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
-    properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "PLAINTEXT");
+  public Future<CommandResult> sendAndWait(Object payload, Metadata metadata) {
+    CommonUtils.validatePayload(payload);
 
-    return properties;
+    ProducerRecord<String, Object> record = new ProducerRecord<>(CommonUtils.getTopicInfo(payload).value(), CommonUtils.getAggregateId(payload), payload);
+
+    if (metadata == null) {
+      metadata = Metadata.builder().build();
+    }
+    metadata.filter().getEntries().entrySet().stream()
+        .filter(entry -> StringUtils.isNoneBlank(entry.getKey(), entry.getValue()))
+        .forEach(entry ->
+            record.headers()
+                .remove(entry.getKey())
+                .add(entry.getKey(), entry.getValue().getBytes(StandardCharsets.UTF_8)));
+
+    return this.sendAndWait(record);
   }
+
+  public Future<CommandResult> sendAndWait(Object payload) {
+    return this.sendAndWait(payload, null);
+  }
+
 }
