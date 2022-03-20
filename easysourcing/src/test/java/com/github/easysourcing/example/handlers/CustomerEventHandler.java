d996ef1160c362994716197f910c255052fcf9a7
akelleci <B>
2022-03-20 23:22:53 +0100

    minor change

---
+++ easysourcing/src/test/java/com/github/easysourcing/example/handlers/CustomerEventHandler.java
@@ -0,0 +1,41 @@
+package com.github.easysourcing.example.handlers;
+
+import com.github.easysourcing.example.domain.CustomerEvent.CreditsAdded;
+import com.github.easysourcing.example.domain.CustomerEvent.CreditsIssued;
+import com.github.easysourcing.example.domain.CustomerEvent.CustomerCreated;
+import com.github.easysourcing.example.domain.CustomerEvent.CustomerDeleted;
+import com.github.easysourcing.example.domain.CustomerEvent.FirstNameChanged;
+import com.github.easysourcing.example.domain.CustomerEvent.LastNameChanged;
+import com.github.easysourcing.messages.Metadata;
+import com.github.easysourcing.messages.events.annotations.HandleEvent;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CustomerEventHandler {
+
+  @HandleEvent
+  public void handle(CustomerCreated event, Metadata metadata) {
+  }
+
+  @HandleEvent
+  public void handle(FirstNameChanged event) {
+  }
+
+  @HandleEvent
+  public void handle(LastNameChanged event) {
+  }
+
+  @HandleEvent
+  public void handle(CreditsAdded event) {
+  }
+
+  @HandleEvent
+  public void handle(CreditsIssued event) {
+  }
+
+  @HandleEvent
+  public void handle(CustomerDeleted event) {
+  }
+
+}
+
