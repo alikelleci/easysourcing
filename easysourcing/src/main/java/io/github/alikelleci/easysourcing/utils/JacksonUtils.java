b542be61d4d9332ed6079439ff6bab995058a0d5
akelleci <B>
2022-04-14 01:59:29 +0200

    minor changes

--- easysourcing/src/main/java/io/github/alikelleci/easysourcing/utils/JacksonUtils.java
+++ easysourcing/src/main/java/io/github/alikelleci/easysourcing/utils/JacksonUtils.java
@@ -19,9 +19,7 @@ public class JacksonUtils {
   public static ObjectMapper enhancedObjectMapper() {
     if (objectMapper == null) {
       objectMapper = new ObjectMapper()
-          .registerModule(new ParameterNamesModule())
-          .registerModule(new Jdk8Module())
-          .registerModule(new JavaTimeModule())
+          .findAndRegisterModules()
           .setSerializationInclusion(JsonInclude.Include.NON_NULL)
           .configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false)
           .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
