cb838a030338f1a8f09fef9e01fb58c01381f6cb
akelleci <B>
2022-04-14 00:46:58 +0200

    minor changes

--- easysourcing/src/test/java/io/github/alikelleci/easysourcing/EasySourcingTest.java
+++ easysourcing/src/test/java/io/github/alikelleci/easysourcing/EasySourcingTest.java
@@ -68,7 +68,7 @@ class EasySourcingTest {
     commands = testDriver.createInputTopic(CustomerCommand.class.getAnnotation(TopicInfo.class).value(),
         new StringSerializer(), CustomSerdes.Json(Command.class).serializer());
 
-    commandResults = testDriver.createOutputTopic(CustomerCommand.class.getAnnotation(TopicInfo.class).value().concat(".resulthandling"),
+    commandResults = testDriver.createOutputTopic(CustomerCommand.class.getAnnotation(TopicInfo.class).value().concat(".results"),
         new StringDeserializer(), CustomSerdes.Json(Command.class).deserializer());
 
     events = testDriver.createOutputTopic(CustomerEvent.class.getAnnotation(TopicInfo.class).value(),
@@ -120,7 +120,7 @@ class EasySourcingTest {
     assertThat(commandResult.getMetadata().get(ID), is(notNullValue()));
     assertThat(commandResult.getMetadata().get(TIMESTAMP), is(notNullValue()));
     assertThat(commandResult.getMetadata().get(RESULT), is("success"));
-    assertThat(commandResult.getMetadata().get(FAILURE), isEmptyOrNullString());
+//    assertThat(commandResult.getMetadata().get(FAILURE), isEmptyOrNullString());
     // Payload
     assertThat(commandResult.getPayload(), is(command.getPayload()));
 
@@ -135,8 +135,8 @@ class EasySourcingTest {
     assertThat(event.getMetadata().get(CORRELATION_ID), is(command.getMetadata().get(CORRELATION_ID)));
     assertThat(event.getMetadata().get(ID), is(notNullValue()));
     assertThat(event.getMetadata().get(TIMESTAMP), is(notNullValue()));
-    assertThat(event.getMetadata().get(RESULT), isEmptyOrNullString());
-    assertThat(event.getMetadata().get(FAILURE), isEmptyOrNullString());
+//    assertThat(event.getMetadata().get(RESULT), isEmptyOrNullString());
+//    assertThat(event.getMetadata().get(FAILURE), isEmptyOrNullString());
     // Payload
     assertThat(event.getPayload(), instanceOf(CustomerCreated.class));
     assertThat(((CustomerCreated) event.getPayload()).getId(), is(((CreateCustomer) command.getPayload()).getId()));
