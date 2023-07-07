89ce6bf2affa49478327edfe0173e2e498c0d761
a.kelleci <a.kelleci@portbase.com>
2023-07-07 21:18:56 +0200

    null safe

--- easysourcing/src/main/java/io/github/alikelleci/easysourcing/messaging/MessageUpcaster.java
+++ easysourcing/src/main/java/io/github/alikelleci/easysourcing/messaging/MessageUpcaster.java
@@ -46,34 +46,54 @@ public class MessageUpcaster implements ValueTransformerWithKey<String, JsonNode
       ObjectNode newMetadata = easySourcing.getObjectMapper().createObjectNode();
 
       // $id
-      String id = oldMetadata.get("entries").get("$id").textValue();
-      ((ObjectNode) root).put("id", id);
-      newMetadata.put("$id", id);
-      ((ObjectNode) oldMetadata.get("entries")).remove("$id");
+      Optional.ofNullable(oldMetadata.get("entries"))
+          .map(node -> node.get("$id"))
+          .map(JsonNode::textValue)
+          .ifPresent(value -> {
+            ((ObjectNode) root).put("id", value);
+            newMetadata.put("$id", value);
+            ((ObjectNode) oldMetadata.get("entries")).remove("$id");
+          });
 
       // $timestamp
       ((ObjectNode) root).put("timestamp", Instant.ofEpochMilli(context.timestamp()).toString());
       newMetadata.put("$timestamp", Instant.ofEpochMilli(context.timestamp()).toString());
 
       // $result
-      String result = oldMetadata.get("entries").get("$result").textValue();
-      newMetadata.put("$result", StringUtils.replace(result, "failed", "failure"));
-      ((ObjectNode) oldMetadata.get("entries")).remove("$result");
+      Optional.ofNullable(oldMetadata.get("entries"))
+          .map(node -> node.get("$result"))
+          .map(JsonNode::textValue)
+          .ifPresent(value -> {
+            newMetadata.put("$result", StringUtils.replace(value, "failed", "failure"));
+            ((ObjectNode) oldMetadata.get("entries")).remove("$result");
+          });
 
       // $failure
-      String failure = oldMetadata.get("entries").get("$failure").textValue();
-      newMetadata.put("$cause", failure);
-      ((ObjectNode) oldMetadata.get("entries")).remove("$failure");
+      Optional.ofNullable(oldMetadata.get("entries"))
+          .map(node -> node.get("$failure"))
+          .map(JsonNode::textValue)
+          .ifPresent(value -> {
+            newMetadata.put("$cause", value);
+            ((ObjectNode) oldMetadata.get("entries")).remove("$failure");
+          });
 
       // $correlationId
-      String correlationId = oldMetadata.get("entries").get("$correlationId").textValue();
-      newMetadata.put("$correlationId", correlationId);
-      ((ObjectNode) oldMetadata.get("entries")).remove("$correlationId");
+      Optional.ofNullable(oldMetadata.get("entries"))
+          .map(node -> node.get("$correlationId"))
+          .map(JsonNode::textValue)
+          .ifPresent(value -> {
+            newMetadata.put("$correlationId", value);
+            ((ObjectNode) oldMetadata.get("entries")).remove("$correlationId");
+          });
 
       // $replyTo
-      String replyTo = oldMetadata.get("entries").get("$replyTo").textValue();
-      newMetadata.put("$replyTo", replyTo);
-      ((ObjectNode) oldMetadata.get("entries")).remove("$replyTo");
+      Optional.ofNullable(oldMetadata.get("entries"))
+          .map(node -> node.get("$replyTo"))
+          .map(JsonNode::textValue)
+          .ifPresent(value -> {
+            newMetadata.put("$replyTo", value);
+            ((ObjectNode) oldMetadata.get("entries")).remove("$replyTo");
+          });
 
       // other metadata info
       Iterator<Map.Entry<String, JsonNode>> it = oldMetadata.get("entries").fields();
