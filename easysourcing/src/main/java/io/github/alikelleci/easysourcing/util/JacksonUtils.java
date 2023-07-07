01117b1cfb3acac65ab183f8f3a2cb0192efe21b
a.kelleci <a.kelleci@portbase.com>
2023-07-07 23:06:51 +0200

    minor change

--- easysourcing/src/main/java/io/github/alikelleci/easysourcing/util/JacksonUtils.java
+++ easysourcing/src/main/java/io/github/alikelleci/easysourcing/util/JacksonUtils.java
@@ -22,7 +22,7 @@ public class JacksonUtils {
           .configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false)
 //          .configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
 //          .configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
-//          .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true)
+          .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
           .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     }
     return objectMapper;
