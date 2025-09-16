69382e83e7aa0631771e059b08f944187f16fc2f
akelleci <a.kelleci@portbase.com>
2025-09-16 11:30:06 +0200

    minor change

--- easysourcing-core/src/main/java/io/github/alikelleci/easysourcing/core/support/serialization/json/JsonSerde.java
+++ easysourcing-core/src/main/java/io/github/alikelleci/easysourcing/core/support/serialization/json/JsonSerde.java
@@ -9,37 +9,37 @@ import org.apache.kafka.common.serialization.Serializer;
 import java.util.Map;
 
 public class JsonSerde<T> implements Serde<T> {
-  private final JsonSerializer<T> jsonSerializer;
-  private final JsonDeserializer<T> jsonDeserializer;
+  private final JsonSerializer<T> serializer;
+  private final JsonDeserializer<T> deserializer;
 
   public JsonSerde(Class<T> targetType) {
     this(targetType, JacksonUtils.enhancedObjectMapper());
   }
 
   public JsonSerde(Class<T> targetType, ObjectMapper objectMapper) {
-    this.jsonSerializer = new JsonSerializer<>(objectMapper);
-    this.jsonDeserializer = new JsonDeserializer<>(targetType, objectMapper);
+    this.serializer = new JsonSerializer<>(objectMapper);
+    this.deserializer = new JsonDeserializer<>(targetType, objectMapper);
   }
 
   @Override
   public Serializer<T> serializer() {
-    return this.jsonSerializer;
+    return this.serializer;
   }
 
   @Override
   public Deserializer<T> deserializer() {
-    return this.jsonDeserializer;
+    return this.deserializer;
   }
 
   @Override
   public void configure(Map<String, ?> configs, boolean isKey) {
-    this.jsonSerializer.configure(configs, isKey);
-    this.jsonDeserializer.configure(configs, isKey);
+    this.serializer.configure(configs, isKey);
+    this.deserializer.configure(configs, isKey);
   }
 
   @Override
   public void close() {
-    this.jsonSerializer.close();
-    this.jsonDeserializer.close();
+    this.serializer.close();
+    this.deserializer.close();
   }
 }
